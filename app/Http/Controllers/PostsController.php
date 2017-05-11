f134caa3068b94a5e3222a79e3954b81ad83132c
Pedro Magalhães <pedromagtaem@gmail.com>
2017-05-11 17:06:48 +0100

    rota auth

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,13 @@ class PostsController extends Controller
 
     public function login()
     {
-       return view('posts.login');
+       return view('auth.login');
+
+    }
+
+    public function register()
+    {
+       return view('auth.register');
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', 'PostsController@index');
 //rota login
 Route::get('/login', 'PostsController@login');
 
+
+//rota cauth
+Route::controllers(['auth' => 'Auth\AuthController'), 'password' => 'Auth\PasswordController'])
+
 //rota detalhes
 Route::get('/list/{id}', 'PrintRequestsController@show')->name('printrequests.show');
 
