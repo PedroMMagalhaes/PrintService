44a67c70bf0c98e5687e9ae3c41696b31fee066b
Manuel Magalhães <2150713@my.ipleria.pt>
2017-05-11 11:26:47 +0100

    Remove requests.

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/login', 'PostsController@login');
 
 //rota detalhes
 Route::get('/printrequests/{id}', 'PrintRequestsController@show')->name('printrequests.show');
+
+//rota impressoes
+Route::get('/printrequests')->middleware('auth');
