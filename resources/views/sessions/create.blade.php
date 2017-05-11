075b274ab0720486b345e169195c010f7b993945
Pedro Magalhães <pedromagtaem@gmail.com>
2017-05-12 00:27:17 +0100

    RegisterLogin_V1

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,19 @@
+@extends ('layout.master')
+
+
+@section('content')
+
+
+  <div class="col-sm-8">
+
+
+<h1> Register</h1>
+<form>
+
+{{ csrf_field() }}
+
+
+</form>
+
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Hamcrest\Core\IsSame;
 
 //rota raiz
 Route::get('/', 'PostsController@index');
@@ -6,13 +7,20 @@ Route::get('/', 'PostsController@index');
 //rota login
 Route::get('/login', 'PostsController@login');
 
-
-//rota cauth
-Route::controllers(['auth' => 'Auth\AuthController'), 'password' => 'Auth\PasswordController'])
+//rota auth
+Auth::routes();
 
 //rota detalhes
 Route::get('/list/{id}', 'PrintRequestsController@show')->name('printrequests.show');
 
 //rota impressoes
 Route::get('/list');
+
 //->middleware('auth');
+//Auth::routes();
+
+//rota login
+Route::get('/register', 'RegistrationController@create' );
+Route::get('/login', 'SessionController@create');
+
+Route::get('/home', 'HomeController@index')->name('home');
