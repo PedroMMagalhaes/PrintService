b6980e696be6356c2f03c1fb4cb2c5bec6196f7b
Manuel Magalhães <2150713@my.ipleria.pt>
2017-05-17 13:52:45 +0100

    Comments nos details v0.5.

--- resources/views/printrequests/details.blade.php
+++ resources/views/printrequests/details.blade.php
@@ -75,6 +75,23 @@
 </div>
   </div>
 
+  <div class="comments">
+      <ul class="list-group">
+
+
+      @foreach ($request->comments as $comment)
+
+      <li class="list-group-item">
+          <strong>
+             
+              {{$comment->created_at}}
+          </strong>
+          {{$comment->comment}}
+      </li>
+      @endforeach
+        </ul>
+  </div>
+
 @endsection
 
 @section ('footer')
