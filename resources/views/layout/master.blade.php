ab77dfd2bfe9c9be0bb2ff3789d4a01c66955e0f
Pedro Magalhães <pedromagtaem@gmail.com>
2017-06-01 11:50:53 +0100

    TesteLoginFixe

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,29 +12,29 @@
   <meta name="generator" content="Codeply">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
-  <!--link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}" /> -->
+  <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}" />
   <link rel="stylesheet" href="{{ URL::asset('css/sidebar.css') }}" />
 </head>
 
 
-@include ('layout.nav')
-@include('layout.sidebar')
 
 <body>
+  @include ('layout.nav')
+  @include('layout.sidebar')
 
 <!--<body>-->
 
 @yield('content')
 
 @include ('layout.footer')
+
+
+  <!--scripts-->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+  <script src="js/scripts.js"></script>
+@yield('scripts')
+
 </body>
-
-
-
-<!--scripts-->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
-<script src="js/scripts.js"></script>
-
 </html>
