19513da2913f409ce29eaae805391642e1005420
Pedro Crespo <2120176@my.ipleiria.pt>
2017-06-02 00:32:13 +0100

    bug fix

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -4,6 +4,16 @@
       <ul class="nav flex-column pl-1">
         @if(!Auth::guest())
           <li class="nav-item"><a class="nav-link" href="{{ url('/list') }}">Request Dashboard</a></li>
+          @if(Auth::user()->admin)
+            <li class="nav-item">
+              <a class="nav-link" href="#submenu2" data-toggle="collapse" data-target="#submenu2">Manage ▾</a>
+              <ul class="list-unstyled flex-column pl-3 collapse" id="submenu2" aria-expanded="false">
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.manageblock') }}">User Clearence</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.managerole') }}">User Roles</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('manageComments') }}">Comments</a></li>
+              </ul>
+            </li>
+          @endif
         @endif
         <li class="nav-item">
           <a class="nav-link" href="#submenu1" data-toggle="collapse" data-target="#submenu1">Statistics by Department ▾</a>
@@ -13,16 +23,7 @@
             @endforeach
           </ul>
         </li>
-        @if(Auth::user()->admin)
-        <li class="nav-item">
-          <a class="nav-link" href="#submenu2" data-toggle="collapse" data-target="#submenu2">Manage ▾</a>
-          <ul class="list-unstyled flex-column pl-3 collapse" id="submenu2" aria-expanded="false">
-            <li class="nav-item"><a class="nav-link" href="{{ route('users.manageblock') }}">User Clearence</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('users.managerole') }}">User Roles</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('manageComments') }}">Comments</a></li>
-          </ul>
-        </li>
-        @endif
+
       </ul>
     </div>
 
